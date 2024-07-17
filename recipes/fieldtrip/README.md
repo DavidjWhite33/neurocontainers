3dc809ae339fa6adf71322bffc7ef61566aea705
David White <davidjwhite33@gmail.com>
2024-07-17 21:01:52 +1000

    update fieldtrip

--- recipes/fieldtrip/README.md
+++ recipes/fieldtrip/README.md
@@ -1,17 +1,19 @@
 
 ----------------------------------
-## fieldtrip/20220617 ##
-Standalone (compiled) version of FieldTrip with MATLAB runtime
+## fieldtrip/20240704 ##
+Standalone (compiled) version of FieldTrip for MATLAB (r2021b) Compiler Runtime (v911)
 
 Example:
 ```
-run_fieldtrip.sh /opt/MCR/v99 yourscript.m
+run_fieldtrip.sh /opt/MCR/v911 yourscript.m
 ```
 
 More documentation can be found here: https://www.fieldtriptoolbox.org/tutorial/
 
-To run applications outside of this container: ml fieldtrip/20220617
+To run applications outside of this container: ml fieldtrip/20240704
 
 Please cite the FieldTrip reference paper when you have used FieldTrip in your study.
 Robert Oostenveld, Pascal Fries, Eric Maris, and Jan-Mathijs Schoffelen. FieldTrip: Open Source Software for Advanced Analysis of MEG, EEG, and Invasive Electrophysiological Data. Computational Intelligence and Neuroscience, vol. 2011, Article ID 156869, 9 pages, 2011. doi:10.1155/2011/156869.
+
+The FieldTrip toolbox is developed and released under the GPL license (see https://www.fieldtriptoolbox.org/copyrights/). The toolbox depends on some external software contributions, which may not be covered under the GPL. All external software that is distributed along with the FieldTrip release version contains an explicit README and an explicit COPYING or LICENSE file that details the copyrights for that specific software and the license under which that specific software is distributed.
 ----------------------------------
--- recipes/fieldtrip/build.sh
+++ recipes/fieldtrip/build.sh
@@ -3,7 +3,7 @@ set -e
 
 # this template file builds fieldtrip and is then used as a docker base image for layer caching
 export toolName='fieldtrip'
-export toolVersion='20220617'
+export toolVersion='20240704'
 # Don't forget to update version change in README.md!!!!!
 
 if [ "$1" != "" ]; then
@@ -11,10 +11,10 @@ if [ "$1" != "" ]; then
     export debug=$1
 fi
 
-
 source ../main_setup.sh
 
-
+export MATLAB_VERSION=2021b
+export MCR_VERSION=v911
 
 neurodocker generate ${neurodocker_buildMode} \
    --base-image ubuntu:18.04 \
@@ -23,11 +23,13 @@ neurodocker generate ${neurodocker_buildMode} \
    --run="chmod +x /usr/bin/ll" \
    --run="mkdir -p ${mountPointList}" \
    --install curl unzip ca-certificates openjdk-8-jre dbus-x11 \
-   --matlabmcr version=2020b install_path=/opt/MCR  \
+   --matlabmcr version=${MATLAB_VERSION} install_path=/opt/mcr  \
    --workdir /opt/${toolName}-${toolVersion}/ \
-   --run="curl -fsSL --retry 5 https://object-store.rc.nectar.org.au/v1/AUTH_dead991e1fa847e3afcca2d3a7041f5d/build/fieldtrip20220617_mcr2020b.tar.gz \
+   --run="curl -fsSL --retry 5 https://object-store.rc.nectar.org.au/v1/AUTH_dead991e1fa847e3afcca2d3a7041f5d/build/${toolName}${toolVersion}_mcr2021b.tar.gz \
       | tar -xz -C /opt/${toolName}-${toolVersion}/ --strip-components 1 \
       && chmod +x /opt/${toolName}-${toolVersion}/*" \
+   --env XAPPLRESDIR=/opt/mcr/${MCR_VERSION}/x11/app-defaults \
+   --env LD_LIBRARY_PATH=/opt/mcr/${MCR_VERSION}/runtime/glnxa64:/opt/mcr/${MCR_VERSION}/bin/glnxa64:/opt/mcr/${MCR_VERSION}/sys/os/glnxa64:/opt/mcr/${MCR_VERSION}/sys/opengl/lib/glnxa64:/opt/mcr/${MCR_VERSION}/extern/bin/glnxa64 \
    --env PATH=/opt/${toolName}-${toolVersion}/:$PATH \
    --env DEPLOY_BINS=run_fieldtrip.sh \
    --copy README.md /README.md \
@@ -36,3 +38,8 @@ neurodocker generate ${neurodocker_buildMode} \
 if [ "$1" != "" ]; then
    ./../main_build.sh
 fi
+
+
+#to copy package archive from local
+#  --copy ${toolName}${toolVersion}_mcr2021b.tar.gz /opt/${toolName}-${toolVersion}.tar.gz \
+#  --run="tar -xzf /opt/${toolName}-${toolVersion}.tar.gz -C /opt/${toolName}-${toolVersion}/ --strip-components 1" \
